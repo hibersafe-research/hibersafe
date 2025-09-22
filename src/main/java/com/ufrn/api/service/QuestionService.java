ce3fb50410fe102fb32a442e9f8a065f1e34e45d
Carlos Antônio <carlosantoniooln@gmail.com>
2025-09-22 08:56:52 -0300

    changes to use embedding on mining

--- src/main/java/com/ufrn/api/service/QuestionService.java
+++ src/main/java/com/ufrn/api/service/QuestionService.java
@@ -117,14 +117,22 @@ public class QuestionService {
 		return new ResponseDTO(topSimilarity, allResults);
 	}
 
-	private List<Double> getTextEmbedding(String text)  {
-		return embeddingClient.embed(text);
-	}
-//	Quando for popular o banco, lembrar de colocar a saída dessa função como campo embedding,
-//	como parâmetro dessa função passar o body
+    /*	Quando for popular o banco, lembrar de colocar a saída dessa função como campo embedding,
+    como parâmetro dessa função passar o body */
+	public List<Double> getTextEmbedding(String text)  {
+        final int MAX_TEXT_LENGTH = 15000;
 
-	public List<QuestionLinkDTO> getQuestionsBySemanticSearch(String mesage) {
-		List<Double> mesageEmbedding = getTextEmbedding(mesage);
-		return questionRepository.findQuestionBySimilarity(mesageEmbedding, MATCH_THRESHOLD, MATCH_CNT);
+        System.out.println("embedding for... " + text.length());
+
+        if (text.length() > MAX_TEXT_LENGTH) {
+            text = text.substring(0, MAX_TEXT_LENGTH);
+        }
+
+        return embeddingClient.embed(text);
+	}
+
+	public List<QuestionLinkDTO> getQuestionsBySemanticSearch(String message) {
+		List<Double> messageEmbedding = getTextEmbedding(message);
+		return questionRepository.findQuestionBySimilarity(messageEmbedding, MATCH_THRESHOLD, MATCH_CNT);
 	}
 }
